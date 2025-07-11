81cc6fbfc147ca8a7a70ee8dbb75db972e259633
Sam Ragsdale <lunesazul@duck.com>
2025-07-11 14:53:10 -0400

    15.7 final submission

--- SDEV200/week5/ch15/src/application/Main.java
+++ SDEV200/week5/ch15/src/application/Main.java
@@ -3,18 +3,35 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			/** create a stack pane, a circle w/ default black
+			* outline & white fill & add the circle to the pane */
+			StackPane pane = new StackPane();
+			Circle circle = new Circle(100);
+			circle.setStroke(Color.BLACK);
+			circle.setFill(Color.WHITE);
+			pane.getChildren().add(circle);
+			
+			// set's the circle object's fill to BLACK when mouse pressed on the pane
+			pane.setOnMousePressed(e -> circle.setFill(Color.BLACK));
+			
+			// set's the circle object's fill to WHITE when mouse released on the pane
+			pane.setOnMouseReleased(e -> circle.setFill(Color.WHITE));
+					
+			// Create a scene & place/show the pane inside of it at 400x300 resolution
+			Scene scene = new Scene(pane, 400, 300);
+			primaryStage.setTitle("Exercise15_7");
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
@@ -24,3 +41,5 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
+
